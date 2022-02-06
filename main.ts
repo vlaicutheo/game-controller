aefce06844a40a2a5a5ca009b0c6756a0055a357
vlaicutheo <95164828+vlaicutheo@users.noreply.github.com>
2022-02-06 16:44:15 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,42 @@
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
     basic.showLeds(`
         . . # . .
-        . . . # .
+        . # . # .
         # # # # #
-        . . . # .
+        . # . # .
         . . # . .
         `)
+    radio.sendString("HONK")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+    radio.sendString("LEDS")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.showLeds(`
+        . . # . #
+        . . . # #
+        # # # # #
+        . . . # #
+        . . # . #
+        `)
+    radio.sendString("STOP")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        # # # # #
+        `)
+    radio.sendString("STOP")
 })
 Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
     basic.showLeds(`
@@ -15,17 +46,23 @@ Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButton
         . # # # .
         . . # . .
         `)
+    radio.sendString("DOWN")
 })
 Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
     basic.showLeds(`
-        . . # . .
-        . # . . .
+        # . # . .
+        # # . . .
         # # # # #
-        . # . . .
-        . . # . .
+        # # . . .
+        # . # . .
         `)
+    radio.sendString("STOP")
 })
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(10)
+    basic.showNumber(10)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -33,6 +70,41 @@ Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButton
         . . # . .
         . . # . .
         `)
+    radio.sendString("UP")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(-10)
+    basic.showNumber(-10)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendString("STOP")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    radio.sendString("RIGHT")
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    radio.sendString("LEFT")
 })
 radio.setGroup(5)
 basic.showIcon(IconNames.Yes)
